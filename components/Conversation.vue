9aa5cbce406fea5f12ebca4fc70bfe187529a697
Anisse <anissbouainbi@hotmail.it>
2024-12-13 22:45:41 +0100

    edit styles conversation

--- components/Conversation.vue
+++ components/Conversation.vue
@@ -1,5 +1,5 @@
 <template>
-    <div @click="select" class="flex items-center bg-neutral-800 hover:bg-violet-900/30 rounded-md space-x-2 px-3 py-2 shadow-md shadow-neutral-950 hover:shadow-violet-800/20 transition duration-500 ease-in-out" :class="{'bg-violet-900/30 ring-1 ring-violet-800/50': isSelected}">
+    <div @click="select" class="flex items-center hover:bg-violet-900/30 rounded-md cursor-pointer space-x-2 px-3 py-2" :class="{'bg-violet-900/30 ring-1 ring-violet-800/50': isSelected}">
         <div v-if="user" class="flex items-center justify-center w-10 h-10 bg-violet-800/50 rounded-full text-violet-300"> {{ store.initials(user) }} </div>
         <p class="text-sm text-neutral-200"> {{ name() }} </p>
     </div>
