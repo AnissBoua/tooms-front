df4221a3223ce57a9eedf894995caa572c043361
Anisse <anissbouainbi@hotmail.it>
2024-12-29 16:29:12 +0100

    responsive signup

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -3,7 +3,7 @@
         <div v-if="!isAuth" class="w-full md:w-1/3 lg:w-1/4 bg-neutral-950 border-r border-neutral-800">
             <Sidebar />
         </div>
-        <div class="absolute z-10 w-full md:static flex-1 bg-neutral-950 transition-all duration-200 ease-in-out" :class="{'left-full': !conversation.mobile, 'left-0': conversation.mobile}">
+        <div class="absolute z-10 w-full md:static flex-1 bg-neutral-950 transition-all duration-200 ease-in-out" :class="{'left-full': auth.user && !conversation.mobile, 'left-0': conversation.mobile}">
             <slot />
         </div>
         <div v-if="incoming" class="fixed z-10 top-0 left-0 right-0 w-max mx-auto">
